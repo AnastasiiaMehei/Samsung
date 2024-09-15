ae85d13c9f6f7f8ea0886a4c7eefed26ba7b7184
AnastasiiaMehei <147993093+AnastasiiaMehei@users.noreply.github.com>
2024-09-15 17:21:44 +0300

    index.html, script.js, styles.css

--- src/script.js
+++ src/script.js
@@ -1,53 +1,78 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-    gsap.registerPlugin(ScrollToPlugin);
+// document.addEventListener("DOMContentLoaded", (event) => {
+//     gsap.registerPlugin(ScrollToPlugin);
 
-    const animationPromise = new Promise((resolve) => {
-        gsap.fromTo([".samsung-logo", ".footer-paragraph", ".save-now"], 
-            { x: -100, opacity: 0 },
-            { x: 0, opacity: 1, duration: 1, onComplete: resolve }
-        );
+//     const animationPromise = new Promise((resolve) => {
+//         gsap.fromTo([".samsung-logo", ".footer-paragraph", ".save-now"], 
+//             { x: -100, opacity: 0 },
+//             { x: 0, opacity: 1, duration: 1, delay: 1, onComplete: resolve }
+//         );
         
-        gsap.fromTo(".samsung-logo",
-            { y: 0, x: 0 },
-            { y: -50, duration: 1.5, delay: 1 }
-        );
+//         gsap.fromTo(".samsung-logo",
+//             { y: 0, x: 0 },
+//             { y: -50, duration: 1, delay: 2 }
+//         );
         
-        gsap.fromTo([".animation-par"], 
-            { x: -100, opacity: 0 },
-            { x: 0, opacity: 1, duration: 1, delay: 2}
-        );
-    });
+//         gsap.fromTo([".animation-par"], 
+//             { x: -100, opacity: 0 },
+//             { x: 0, opacity: 1, duration: 1, delay: 3}
+//         );
+//     });
 
-    animationPromise.then(() => {
-        // Додаткова затримка після завершення анімації
-        setTimeout(() => {
-            const sectionFrame1 = document.querySelector('.section-frame1');
-            if (sectionFrame1) {
-                sectionFrame1.style.display = 'none';
-            }
+//     animationPromise.then(() => {
+//         // Додаткова затримка після завершення анімації
+//         setTimeout(() => {
+//             const sectionFrame1 = document.querySelector('.section-frame1');
+//             if (sectionFrame1) {
+//                 sectionFrame1.style.display = 'none';
+//             }
 
-            const sectionFrame = document.querySelector('.section-frame');
-            if (sectionFrame) {
-                sectionFrame.style.display = 'flex';
-            }
+//             const sectionFrame = document.querySelector('.section-frame');
+//             if (sectionFrame) {
+//                 sectionFrame.style.display = 'flex';
+//                 gsap.fromTo(".frame", 
+//                     { x: -100, opacity: 0 },
+//                     { x: 0, opacity: 1, duration: 1 }
+//                 );
+//             }
+//         }, 4000); // Затримка на 1 секунду
+//     });
+// // =============== SLIDER ==========================
+// // animationPromise.then(() => {
+// //     // Ініціалізація Swiper
+// //     const swiper = new Swiper('.swiper', {
+// //         navigation: {
+// //             nextEl: '.swiper-button-next',
+// //             prevEl: '.swiper-button-prev',
+// //         },
+// //         on: {
+// //             slideChange: function () {
+// //                 const currentSlide = this.activeIndex;
+// //                 const sections = document.querySelectorAll('.section-frame');
+                
+// //                 sections.forEach((section, index) => {
+// //                     if (index === currentSlide) {
+// //                         section.style.display = 'flex';
+// //                     } else {
+// //                         section.style.display = 'none';
+// //                     }
+// //                 });
+//     //         },
+//     //     },
+//     // });
 
-            // Додаткова логіка після зміни стилів
-            console.log('Animation completed. Styles changed.');
-        }, 3000); // Затримка на 1 секунду
-    });
-});
-    
-        // Обробник події для кнопки "Read more..."
-        // const readMoreLink = document.querySelector('.read-more a');
-        // if (readMoreLink) {
-        //     readMoreLink.addEventListener('click', (e) => {
-        //         e.preventDefault(); // Запобігає стандартному поведінці посилання
-    
-        //         // Відкриття нової порожньої сторінки
-        //         window.open('', '_blank');
-    
-        //         // Додаткова логіка при натисканні кнопки
-        //         console.log('Read more button clicked. New page opened.');
-        //     });
-        // }
-    
+
+
+// // // ==============
+// //   const readMoreLink = document.querySelector('.read-more a');
+// //   if (readMoreLink) {
+// //       readMoreLink.addEventListener('click', (e) => {
+// //           e.preventDefault(); // Запобігає стандартному поведінці посилання
+
+// //           // Відкриття нової порожньої сторінки
+// //           window.open('', '_blank');
+
+// //           // Додаткова логіка при натисканні кнопки
+// //           console.log('Read more button clicked. New page opened.');
+// //       });
+// //   }
+// });

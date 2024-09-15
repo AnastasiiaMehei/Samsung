969090a5c8860df7925122803b4df9fb1f626445
AnastasiiaMehei <147993093+AnastasiiaMehei@users.noreply.github.com>
2024-09-15 15:06:27 +0300

    styles.css, script.js, index.html

--- src/script.js
+++ src/script.js
@@ -1,50 +1,53 @@
-    // //   document.addEventListener("DOMContentLoaded", (event) => {
-    // //     gsap.registerPlugin(ScrollToPlugin)
-    // //     gsap.fromTo([".samsung-logo", ".footer-paragraph", ".save-now"], 
-    // //         { x: -100, opacity: 0 },
-    // // { x: 0, opacity: 1, duration: 1});  
-    // //         gsap.fromTo(".samsung-logo",
-    // //             { y: 0, x: 0 },
-    // //             { y: -50, duration: 1.5, delay: 1 }
-    // //         );
-    // //         gsap.fromTo(".div-paragraph", 
-    // //             { x: -100, opacity: 0 },
-    // //     { x: 0, opacity: 1, duration: 1, delay: 2});  
-    // // //     gsap.fromTo(".frame2", 
-    // // //         { x: -100, opacity: 0 },
-    // // // { x: 0, opacity: 1, duration: 1, delay: 3});  
-    // //     });
-    // document.addEventListener("DOMContentLoaded", (event) => {
-    //     gsap.registerPlugin(ScrollToPlugin);
-    
-    //     const animationPromise = new Promise((resolve) => {
-    //         gsap.fromTo([".samsung-logo", ".footer-paragraph", ".save-now"], 
-    //             { x: -100, opacity: 0 },
-    //             { x: 0, opacity: 1, duration: 1, onComplete: resolve }
-    //         );
-            
-    //         gsap.fromTo(".samsung-logo",
-    //             { y: 0, x: 0 },
-    //             { y: -50, duration: 1.5, delay: 1 }
-    //         );
-            
-    //         gsap.fromTo(".div-paragraph", 
-    //             { x: -100, opacity: 0 },
-    //             { x: 0, opacity: 1, duration: 1, delay: 2}
-    //         );
-    //     });
-    
-    //     animationPromise.then(() => {
-    //         // Зміна стилів після завершення анімації
+document.addEventListener("DOMContentLoaded", (event) => {
+    gsap.registerPlugin(ScrollToPlugin);
+
+    const animationPromise = new Promise((resolve) => {
+        gsap.fromTo([".samsung-logo", ".footer-paragraph", ".save-now"], 
+            { x: -100, opacity: 0 },
+            { x: 0, opacity: 1, duration: 1, onComplete: resolve }
+        );
+        
+        gsap.fromTo(".samsung-logo",
+            { y: 0, x: 0 },
+            { y: -50, duration: 1.5, delay: 1 }
+        );
+        
+        gsap.fromTo([".animation-par"], 
+            { x: -100, opacity: 0 },
+            { x: 0, opacity: 1, duration: 1, delay: 2}
+        );
+    });
+
+    // animationPromise.then(() => {
+    //     // Додаткова затримка після завершення анімації
+    //     setTimeout(() => {
     //         const sectionFrame1 = document.querySelector('.section-frame1');
     //         if (sectionFrame1) {
-    //             sectionFrame1.style.display = 'flex';
+    //             sectionFrame1.style.display = 'none';
     //         }
-    
-    //         const sectionFrame2 = document.querySelector('.section-frame2');
-    //         if (sectionFrame2) {
-    //             sectionFrame2.style.display = 'none';
+
+    //         const sectionFrame = document.querySelector('.section-frame');
+    //         if (sectionFrame) {
+    //             sectionFrame.style.display = 'flex';
     //         }
-    //     });
+
+    //         // Додаткова логіка після зміни стилів
+    //         console.log('Animation completed. Styles changed.');
+    //     }, 1000); // Затримка на 1 секунду
     // });
+});
+    
+        // Обробник події для кнопки "Read more..."
+        // const readMoreLink = document.querySelector('.read-more a');
+        // if (readMoreLink) {
+        //     readMoreLink.addEventListener('click', (e) => {
+        //         e.preventDefault(); // Запобігає стандартному поведінці посилання
+    
+        //         // Відкриття нової порожньої сторінки
+        //         window.open('', '_blank');
+    
+        //         // Додаткова логіка при натисканні кнопки
+        //         console.log('Read more button clicked. New page opened.');
+        //     });
+        // }
     
